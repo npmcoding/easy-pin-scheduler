272647921773937410492dd6fa68ac4aaf54f173
npmcoding <34496277+npmcoding@users.noreply.github.com>
2019-12-27 10:53:23 -0600

    completed signup page functionality

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -5,11 +5,12 @@ import {
     FormControl,
     ControlLabel
 } from "react-bootstrap";
+import { Auth } from "aws-amplify";
 import LoaderButton from "../../components/LoaderButton/LoaderButton";
 import { useFormFields } from "../../libs/hooksLib";
 import "./Signup.css";
 
-export default props => {
+export default ({ setUserHasAuthenticated, history }) => {
     const [{ email, password, confirmPassword, confirmationCode }, handleFieldChange] = useFormFields({
         email: "",
         password: "",
@@ -22,13 +23,52 @@ export default props => {
     const handleSubmit = async e => {
         e.preventDefault();
         setIsLoading(true);
-        setNewUser("test");
-        setIsLoading(false);
+
+        try {
+            const newUser = await Auth.signUp({
+                username: email,
+                password: password
+            });
+            setIsLoading(false);
+            setNewUser(newUser);
+        } catch (e) {
+            // breaks if user refreshes before confirmation
+            // https://serverless-stack.com/chapters/signup-with-aws-cognito.html
+
+            if (e.code === "UsernameExistsException") {
+                handleUsernameExistsException();
+            } else {
+                alert(e.message);
+                setIsLoading(false);
+            }
+        }
+    }
+
+    const handleUsernameExistsException = async () => {
+        try {
+            const newUser = await Auth.resendSignUp(email);
+            setIsLoading(false);
+            setNewUser(newUser);
+        } catch (e) {
+            alert(e.message);
+            setIsLoading(false);
+        }
     }
 
     const handleConfirmationSubmit = async e => {
         e.preventDefault();
         setIsLoading(true);
+
+        try {
+            await Auth.confirmSignUp(email, confirmationCode);
+            await Auth.signIn(email, password);
+
+            setUserHasAuthenticated(true);
+            history.push("/");
+        } catch (e) {
+            alert(e.message);
+            setIsLoading(false);
+        }
     }
 
     const renderConfirmationForm = () => {
@@ -62,33 +102,33 @@ export default props => {
                 <FormGroup controlId="email" bsSize="large">
                     <ControlLabel>Email</ControlLabel>
                     <FormControl
-                    autoFocus
-                    type="email"
-                    value={email}
-                    onChange={handleFieldChange}
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={handleFieldChange}
                     />
                 </FormGroup>
                 <FormGroup controlId="password" bsSize="large">
                     <ControlLabel>Password</ControlLabel>
                     <FormControl
-                    type="password"
-                    value={password}
-                    onChange={handleFieldChange}
+                        type="password"
+                        value={password}
+                        onChange={handleFieldChange}
                     />
                 </FormGroup>
                 <FormGroup controlId="confirmPassword" bsSize="large">
                     <ControlLabel>Confirm Password</ControlLabel>
                     <FormControl
-                    type="password"
-                    onChange={handleFieldChange}
-                    value={confirmPassword}
+                        type="password"
+                        onChange={handleFieldChange}
+                        value={confirmPassword}
                     />
                 </FormGroup>
                 <LoaderButton
-                block
-                type="submit"
-                bsSize="large"
-                isLoading={isLoading}
+                    block
+                    type="submit"
+                    bsSize="large"
+                    isLoading={isLoading}
                 >Signup</LoaderButton>
             </form>
         );
