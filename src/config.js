ce5123e9b6a122df740bd8ed7c60965fb7f408de
Nicholas <nicholas.p.martin@gmail.com>
2020-06-25 16:45:22 -0500

    refactor config file

--- src/config.js
+++ src/config.js
@@ -1,18 +1,25 @@
+export const MAX_ATTACHMENT_SIZE = 5000000;
+export const PINTEREST_APP_ID = "4988587254885678740";
+export const s3 = {
+    REGION: "us-east-2",
+    BUCKET: "easy-pin-scheduler",
+    IMG_BUCKET: "eps-images"
+};
+export const apiGateway = {
+    REGION: "us-east-2",
+    URL: "https://1w1s2iy56d.execute-api.us-east-2.amazonaws.com/dev"
+};
+export const cognito = {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_MngmmIPv7",
+    APP_CLIENT_ID: "792hha7d95psgng6l5v7vqm2kt",
+    IDENTITY_POOL_ID: "us-east-2:79595818-51ea-41b6-8002-f5a694266fee"
+}
+
 export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
-    PINTEREST_APP_ID: "4988587254885678740",
-    s3: {
-        REGION: "us-east-2",
-        BUCKET: "easy-pin-scheduler"
-    },
-    apiGateway: {
-        REGION: "us-east-2",
-        URL: "https://1w1s2iy56d.execute-api.us-east-2.amazonaws.com/dev"
-    },
-    cognito: {
-        REGION: "us-east-2",
-        USER_POOL_ID: "us-east-2_MngmmIPv7",
-        APP_CLIENT_ID: "792hha7d95psgng6l5v7vqm2kt",
-        IDENTITY_POOL_ID: "us-east-2:79595818-51ea-41b6-8002-f5a694266fee"
-    }
+    MAX_ATTACHMENT_SIZE,
+    PINTEREST_APP_ID,
+    s3,
+    apiGateway,
+    cognito
 };
